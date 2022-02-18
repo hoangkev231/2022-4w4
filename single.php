67cb20654791bd3c22fcf8be7d8826e819ac7db0
hoangkev231 <hoangkev231@gmail.com>
2022-02-18 12:33:18 -0500

    commit de lab-4 (cours du 17/2/2022)

---
+++ single.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package underscore
+ */
+
+get_header();
+?>
+
+<main class="site__main">
+	<?php
+		if(have_posts()):the_post();
+	?>
+	<?php
+		the_title();
+	?>
+	<?php
+		the_content();
+	?>
+	<?php
+		endif;
+	?>
+</main>
+<?php
+	get_footer();
+?>
